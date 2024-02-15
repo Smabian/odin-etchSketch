f9a9369027d32138807df7deda3b8a0e07e5d063
Smabian <5991984+Smabian@users.noreply.github.com>
2024-02-15 19:08:51 +0100

    Add hover effect to each div element in grid

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 
 function createGrid () {
 
-    for(i=0;i<16;i++){
+    for(let i=0;i<16;i++){
         let gridItem = document.createElement("div");
         let gridRoot = document.getElementById("container");
         gridRoot.appendChild(gridItem);
-        console.log("Grid #"+i+" created");
     }
 }
 
+function hoverEffect (item, color) {
+    gridItem[item].style.backgroundColor = color;
+}
+
 createGrid();
+
+let gridItem = document.querySelectorAll("#container div");
+
+counter = gridItem.length;
+for (let i=0;i<counter;i++) {
+   gridItem[i].addEventListener("mouseover", function() {
+        hoverEffect(i,'blue');
+   });
+}
