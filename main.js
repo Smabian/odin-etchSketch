1468f8e39103166c99bb5fc94d14323f07684624
Smabian <5991984+Smabian@users.noreply.github.com>
2024-02-15 20:13:49 +0100

    Set grid size dynamically based on user input

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 
 function createGrid (gridsize) {
 
+    let itemSize = calculateGridItemSize(gridsize);
+    
     for(let i=0;i<gridsize;i++){
         let gridItem = document.createElement("div");
+        gridItem.style.width = itemSize + "px";
+        gridItem.style.height = itemSize + "px";
         let gridRoot = document.getElementById("container");
         gridRoot.appendChild(gridItem);
     }
 
-    let gridSelection = document.querySelectorAll("#container div");
+    let gridSelection = document.querySelectorAll("#container div");    
     
     counter = gridSelection.length;
     for (let i=0;i<counter;i++) {
@@ -20,6 +24,13 @@ function createGrid (gridsize) {
 
 function hoverEffect (item, color) {
     item.style.backgroundColor = color;
+}
+
+function calculateGridItemSize (gridSize) {
+    let itemPerRow = Math.sqrt(gridSize);
+    console.log(itemPerRow);
+    let itemSize = 960 / itemPerRow;
+    return itemSize;
 }
 
 let sizeButton = document.getElementById("sizeButton");
