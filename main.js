6a2458bfa9a3b84ca207a26b26a47eb92e493a26
Smabian <5991984+Smabian@users.noreply.github.com>
2024-02-15 19:49:55 +0100

    Add Button to set number of grids

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
 
-function createGrid () {
+function createGrid (gridsize) {
 
-    for(let i=0;i<16;i++){
+    for(let i=0;i<gridsize;i++){
         let gridItem = document.createElement("div");
         let gridRoot = document.getElementById("container");
         gridRoot.appendChild(gridItem);
     }
+
+    let gridSelection = document.querySelectorAll("#container div");
+    
+    counter = gridSelection.length;
+    for (let i=0;i<counter;i++) {
+    gridSelection[i].addEventListener("mouseover", function() {
+            hoverEffect(gridSelection[i],'blue');
+    });
+}
+
 }
 
 function hoverEffect (item, color) {
-    gridItem[item].style.backgroundColor = color;
+    item.style.backgroundColor = color;
 }
 
-createGrid();
+let sizeButton = document.getElementById("sizeButton");
 
-let gridItem = document.querySelectorAll("#container div");
-
-counter = gridItem.length;
-for (let i=0;i<counter;i++) {
-   gridItem[i].addEventListener("mouseover", function() {
-        hoverEffect(i,'blue');
-   });
-}
+sizeButton.addEventListener("click", function (){
+    let gridSize = prompt("Enter the number of Grids you want to use");
+    createGrid(gridSize);
+});
